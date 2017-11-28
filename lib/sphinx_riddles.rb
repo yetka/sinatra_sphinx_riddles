574404120c460353bce5d1d030d0a6b257300482
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-28 09:11:50 -0800

    add asteroids riddle to test

--- lib/sphinx_riddles.rb
+++ lib/sphinx_riddles.rb
@@ -1,16 +1,25 @@
 class Riddle
-  def initialize(question,answer)
+  def initialize (question, answer)
     @question = question
     @answer = answer
   end
 
   def answer_checker
-    if @question == "When I am young, I go on four legs, then I go on two legs, then I go on three legs, and then I die."
-      if @answer == "human"
+    if (@question == "When I am young, I go on four legs, then I go on two legs, then I go on three legs, and then I die.")
+      if (@answer == "human")
+        "Your answer is correct"
+      else
+        "Your answer is wrong"
+      end
+    
+
+    elsif (@question == "Large as a mountain, small as a pea, Endlessly swimming in a waterless sea.")
+      if (@answer == "asteroids")
         "Your answer is correct"
       else
         "Your answer is wrong"
       end
     end
+
   end
 end
--- spec/sphinx_riddles_spec.rb
+++ spec/sphinx_riddles_spec.rb
@@ -3,14 +3,17 @@ require('sphinx_riddles')
 
 describe(Riddle) do
   describe("answer_checker") do
-    it("returns false if the answer is tree") do
-      new_riddle = Riddle.new("When I am young, I go on four legs, then I go on two legs, then I go on three legs, and then I die.","tree")
-      expect(new_riddle.answer_checker()).to(eq("Your answer is wrong"))
-    end
 
     it("returns true if the answer is human") do
       new_riddle = Riddle.new("When I am young, I go on four legs, then I go on two legs, then I go on three legs, and then I die.","human")
       expect(new_riddle.answer_checker()).to(eq("Your answer is correct"))
     end
+
+    it("returns true if the answer is asteroids") do
+      new_riddle = Riddle.new("Large as a mountain, small as a pea, Endlessly swimming in a waterless sea.","asteroids")
+      expect(new_riddle.answer_checker()).to(eq("Your answer is correct"))
+    end
+
+
   end
 end
